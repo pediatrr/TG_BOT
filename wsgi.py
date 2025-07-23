346a94a4ed689be8f95b9800de3076e68f9ba85b
Kristina <133504248+pediatrr@users.noreply.github.com>
2025-07-23 22:35:57 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,13 @@
 from DocDeti_Bot import app as application
 
 if __name__ == "__main__":
-    application.run()
+    # Этот блок нужен только для локального тестирования
+    from threading import Thread
+    from DocDeti_Bot import bot
+    
+    Thread(target=lambda: application.run(
+        host='0.0.0.0',
+        port=int(os.getenv('PORT', 5000))
+    )).start()
+    
+    bot.run()
